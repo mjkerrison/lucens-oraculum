2f9e6ab27064c372b7f572d440ced70da533a5b8
mjkerrison <mjkerrison@users.noreply.github.com>
2025-01-29 14:31:12 +1100

    Setup

---
+++ notes.R
@@ -0,0 +1,10 @@
+
+# https://github.com/posit-dev/shinychat
+# https://github.com/julianschmocker/shinyChatR
+# https://blogs.rstudio.com/ai/posts/2024-04-04-chat-with-llms-using-chattr/
+# https://github.com/jimbrig/r-shiny-llm-chat
+# https://github.com/noclocks/demo-rshiny-assistant
+# https://ellmer.tidyverse.org/
+# https://www.r-bloggers.com/2024/06/large-language-models-llms-in-r-with-tidychatmodels/
+# https://mlverse.github.io/chattr/reference/index.html
+# https://github.com/wch/llm-chat
